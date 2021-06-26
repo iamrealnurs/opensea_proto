1432bdf6f884b709e78e969c120e1142e786945d
iamrealnurs <nurs567@yandex.ru>
2021-06-26 16:58:39 +0600

    add bootstrap and create main app

---
+++ src/opensea/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+    name = "Nurs"
+
+    return render(request, 'home.html', {'name':name})
+
+def about(request):
+    name = "About"
+
+    return render(request, 'about.html', {'name':name})
+
