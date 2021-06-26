d4736233ab95d99b77e408c193b3f4c74f9af378
iamrealnurs <nurs567@yandex.ru>
2021-06-26 17:30:40 +0600

    Adding product model to admin

--- src/main/models.py
+++ src/main/models.py
@@ -1,3 +1,16 @@
+from datetime import timezone
+
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Товар"
+        verbose_name_plural = "Товары"
+        ordering = ['id']
